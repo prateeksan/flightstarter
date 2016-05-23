fe3a50ffe6347ce5620362bfb89248ca79156eb8
prateeksan <sanyal.prateek@gmail.com>
2016-05-23 16:13:34 -0400

    invalid registration spec added (wip #13)

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -23,4 +23,24 @@ RSpec.feature 'New User Registration' do
       expect(page).to have_text("Welcome testuser!")
   end
 
+  scenario 'A new user registers with invalid fields' do
+      visit '/'
+      click_link "Sign Up"
+
+      fill_in "Username", :with => "testuser"
+      fill_in "Password", :with => ""
+      fill_in "Email", :with => "test@user.com"
+      fill_in "Age", :wtih => "20"
+      fill_in "Pronoun", :with => "He"
+      fill_in "Zip", :with => "122011"
+      fill_in "City", :with => "Delhi"
+      fill_in "Street Address", :with => "5 Street West"
+      fill_in "Biodata", :with => "All you need to know about me."
+
+      #TODO(soon): Add spec tests for Avatar upload
+
+      click_button "Sign Up"
+      expect(page).to have_text("The following errors occured:")
+  end
+
 end
