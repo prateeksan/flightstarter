7707ecca4333d4aa26c57e136c82d7b8b04dc988
Steven Dee <steve@smartercode.net>
2016-05-23 15:09:35 -0400

    Valid and invalid input for campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -4,11 +4,21 @@ class CampaignsController < ApplicationController
   end
 
   def create
-    flash[:success] = 'Campaign was successfully created.'
+    @campaign = Campaign.create(campaign_params)
+    if @campaign.save
+      flash[:success] = 'Campaign was successfully created.'
+    else
+      flash[:error] = 'There was an error creating the campaign.'
+    end
     redirect_to campaigns_path
   end
 
   def index
     @campaigns = Campaign.all
   end
+
+  private
+    def campaign_params
+      params.require(:campaign).permit(:title, :description, :requisite_funding)
+    end
 end
--- spec/features/creating_campaigns_spec.rb
+++ spec/features/creating_campaigns_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
-RSpec.feature "Creating campaigns", type: :feature do
-  scenario 'User creates a campaign' do
+RSpec.feature "User creates a campaign", type: :feature do
+  scenario 'with minimal input' do
     visit '/campaigns/new'
-
     fill_in 'Title', with: 'Cool Campaign'
     click_button 'Create Campaign'
+    expect(page).to have_text('Campaign was successfully created.')
+  end
 
+  scenario 'with invalid input' do
+    visit '/campaigns/new'
+    click_button 'Create Campaign'
+    expect(page).to have_text('There was an error creating the campaign.')
+  end
+
+  scenario 'with detailed input' do
+    visit '/campaigns/new'
+    fill_in 'Title', with: 'My Campaign'
+    fill_in 'Description', with: 'Some really neat description.'
+    fill_in 'Requisite funding', with: '350'
+    click_button 'Create Campaign'
     expect(page).to have_text('Campaign was successfully created.')
   end
 end
