c01fa3370150d9965bf457e8f27c98a7f0cb1843
Steven Dee <steve@smartercode.net>
2016-05-25 17:05:40 -0400

    Work a bit with cities and locations
    Makes geocoding work as well.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,12 @@
 class City < ActiveRecord::Base
+  geocoded_by :name_with_country
+  after_validation :geocode
+
+  def name_with_country
+    if country
+      "#{name}, #{country}"
+    else
+      name
+    end
+  end
 end
