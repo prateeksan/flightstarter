6121cdde2a86eea9bc7228c0b64257248b730a2a
Steven Dee <steve@smartercode.net>
2016-05-26 16:40:10 -0400

    find_by instead of find
    find_by returns nil on failure instead of raising.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      return User.find(session[:user_id])
+      return User.find_by id: session[:user_id]
     else
       return nil
     end
