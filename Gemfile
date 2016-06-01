3e3c8f8d76e6345db529c339db3368df77d8d1d1
Steven Dee <steve@smartercode.net>
2016-06-01 17:01:37 -0400

    Prep app for heroku deployment
    - Use postgresql as the db. This required changing one of the
      migrations.
    
    - Use puma as the web server.
    
    - Add rack-timeout.

--- Gemfile
+++ Gemfile
@@ -2,10 +2,14 @@ source 'https://rubygems.org'
 ruby '2.3.1'
 
 gem 'rails', '4.2.6'
-gem 'sqlite3'
+gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
+
+gem 'puma'
+gem 'rack-timeout'
+gem 'rails_12factor', group: :production
 
 gem 'geocoder'
 gem 'wepay'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525205724) do
+ActiveRecord::Schema.define(version: 20160528214721) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "campaigns", force: :cascade do |t|
     t.string   "title"
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 20160525205724) do
     t.integer  "user_id"
   end
 
-  add_index "campaigns", ["user_id"], name: "index_campaigns_on_user_id"
+  add_index "campaigns", ["user_id"], name: "index_campaigns_on_user_id", using: :btree
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -51,18 +54,18 @@ ActiveRecord::Schema.define(version: 20160525205724) do
     t.datetime "updated_at",     null: false
   end
 
-  add_index "legs", ["campaign_id"], name: "index_legs_on_campaign_id"
+  add_index "legs", ["campaign_id"], name: "index_legs_on_campaign_id", using: :btree
 
   create_table "locations", force: :cascade do |t|
     t.string   "title"
-    t.float    "latitude"
-    t.float    "longitude"
     t.integer  "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
-  add_index "locations", ["city_id"], name: "index_locations_on_city_id"
+  add_index "locations", ["city_id"], name: "index_locations_on_city_id", using: :btree
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "user_id"
@@ -74,8 +77,8 @@ ActiveRecord::Schema.define(version: 20160525205724) do
     t.datetime "updated_at",                           null: false
   end
 
-  add_index "pledges", ["campaign_id"], name: "index_pledges_on_campaign_id"
-  add_index "pledges", ["user_id"], name: "index_pledges_on_user_id"
+  add_index "pledges", ["campaign_id"], name: "index_pledges_on_campaign_id", using: :btree
+  add_index "pledges", ["user_id"], name: "index_pledges_on_user_id", using: :btree
 
   create_table "residences", force: :cascade do |t|
     t.string   "name"
@@ -85,7 +88,7 @@ ActiveRecord::Schema.define(version: 20160525205724) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "residences", ["location_id"], name: "index_residences_on_location_id"
+  add_index "residences", ["location_id"], name: "index_residences_on_location_id", using: :btree
 
   create_table "stations", force: :cascade do |t|
     t.string   "name"
@@ -95,7 +98,19 @@ ActiveRecord::Schema.define(version: 20160525205724) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "stations", ["location_id"], name: "index_stations_on_location_id"
+  add_index "stations", ["location_id"], name: "index_stations_on_location_id", using: :btree
+
+  create_table "stops", force: :cascade do |t|
+    t.integer  "campaign_id", null: false
+    t.integer  "city_id",     null: false
+    t.integer  "leg_id",      null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "stops", ["campaign_id"], name: "index_stops_on_campaign_id", using: :btree
+  add_index "stops", ["city_id"], name: "index_stops_on_city_id", using: :btree
+  add_index "stops", ["leg_id"], name: "index_stops_on_leg_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -113,7 +128,16 @@ ActiveRecord::Schema.define(version: 20160525205724) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "users", ["city_id"], name: "index_users_on_city_id"
-  add_index "users", ["location_id"], name: "index_users_on_location_id"
+  add_index "users", ["city_id"], name: "index_users_on_city_id", using: :btree
+  add_index "users", ["location_id"], name: "index_users_on_location_id", using: :btree
 
+  add_foreign_key "campaigns", "users"
+  add_foreign_key "legs", "campaigns"
+  add_foreign_key "locations", "cities"
+  add_foreign_key "pledges", "campaigns"
+  add_foreign_key "pledges", "users"
+  add_foreign_key "residences", "locations"
+  add_foreign_key "stations", "locations"
+  add_foreign_key "users", "cities"
+  add_foreign_key "users", "locations"
 end
