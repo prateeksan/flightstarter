15825ae838e9b37ac44c92c5de167d7f5b98aa40
Steven Dee <steve@smartercode.net>
2016-06-07 15:13:35 -0400

    Add FULL_HOST to production env
    Needed for Stripe redirect_uri.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,3 +80,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+if ENV['FULL_HOST']
+  OmniAuth.config.full_host = ENV['FULL_HOST']
+else
+  Rails.logger.warn 'FULL_HOST not set'
+end
